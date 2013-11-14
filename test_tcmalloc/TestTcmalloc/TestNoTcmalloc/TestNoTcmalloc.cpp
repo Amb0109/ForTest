290f132ee6cd023255933008b1aeae7e873d56d5
Amb <amb.main@gmail.com>
2013-11-14 14:17:39 +0800

    tcmalloc测试

---
+++ test_tcmalloc/TestTcmalloc/TestNoTcmalloc/TestNoTcmalloc.cpp
@@ -0,0 +1,15 @@
+// TestNoTcmalloc.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "test_func.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int ti = clock();
+	//random_malloc_free(1000000, 1024, 102400);
+	random_malloc_free_mthd(3, 1000000, 1024, 102400);
+	printf("%d\n", clock() - ti);
+	return 0;
+}
+
